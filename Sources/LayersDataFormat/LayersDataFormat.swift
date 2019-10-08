c1a5ee9d2aef8551d278084b50d62a666ed73149
mikowals <mikowals@gmail.com>
2019-10-08 11:32:29 +1000

    add .convolved2DDF and .averagePooledDF

--- Sources/LayersDataFormat/LayersDataFormat.swift
+++ Sources/LayersDataFormat/LayersDataFormat.swift
@@ -1,3 +1,160 @@
-struct LayersDataFormat {
-    var text = "Hello, World!"
+import TensorFlow
+
+public extension Padding {
+    @inlinable
+    internal var raw: Raw.Padding {
+        switch self {
+        case .same: return .same
+        case .valid: return .valid
+        }
+    }
+
+    @inlinable
+    internal var raw2: Raw.Padding2 {
+        switch self {
+        case .same: return .same
+        case .valid: return .valid
+        }
+    }
+}
+
+public extension Tensor where Scalar: TensorFlowFloatingPoint {
+    /// TensorFlow builtin conv2d gradient helper for the input.
+    @inlinable
+    //@differentiable(wrt: (self, filter), vjp: _vjpConv2DBackpropInput)
+    internal func conv2DBackpropInputDF(
+        shape: Tensor<Int32>,
+        filter: Tensor,
+        strides: (Int, Int, Int, Int),
+        padding: Padding,
+        dataFormat: Raw.DataFormat = .nhwc
+    ) -> Tensor {
+        return Raw.conv2DBackpropInput(
+            inputSizes: shape,
+            filter: filter,
+            outBackprop: self,
+            strides: [Int32(strides.0), Int32(strides.1), Int32(strides.2), Int32(strides.3)],
+            padding: padding.raw2,
+            explicitPaddings: [],
+            dataFormat: dataFormat)
+    }
+
+    /// TensorFlow builtin conv2d gradient helper for the filter.
+    @inlinable
+    //@differentiable(wrt: (self, input), vjp: _vjpConv2DBackpropFilter)
+    internal func conv2DBackpropFilterDF(
+        input: Tensor,
+        filterSizes: Tensor<Int32>,
+        strides: (Int, Int, Int, Int),
+        padding: Padding,
+        dataFormat: Raw.DataFormat = .nhwc
+    ) -> Tensor {
+        return Raw.conv2DBackpropFilter(
+            input,
+            filterSizes: filterSizes,
+            outBackprop: self,
+            strides: [Int32(strides.0), Int32(strides.1), Int32(strides.2), Int32(strides.3)],
+            padding: padding.raw2,
+            explicitPaddings: [],
+            dataFormat: dataFormat
+        )
+    }
+    
+    @inlinable
+    internal func _vjpConvolved2DDF(
+        filter: Tensor,
+        strides: (Int, Int, Int, Int),
+        padding: Padding,
+        dataFormat: Raw.DataFormat = .nhwc
+    ) -> (Tensor, (Tensor) -> (Tensor, Tensor)) {
+        let value = convolved2DDF(withFilter: filter, strides: strides,
+                                padding: padding, dataFormat: dataFormat)
+        return (value, { v in
+            return (
+                v.conv2DBackpropInputDF(
+                    shape: self.shapeTensor, filter: filter,
+                    strides: strides, padding: padding, dataFormat: dataFormat
+                ),
+                v.conv2DBackpropFilterDF(
+                    input: self, filterSizes: filter.shapeTensor,
+                    strides: strides, padding: padding, dataFormat: dataFormat
+                )
+            )
+        })
+    }
+    @inlinable
+    internal func _vjpAveragePooledDF(
+        kernelSize: (Int, Int, Int, Int),
+        strides: (Int, Int, Int, Int),
+        padding: Padding,
+        dataFormat: Raw.DataFormat = .nhwc
+    ) -> (Tensor, (Tensor) -> Tensor) {
+        // TODO: Currently this is not higher order differentiable. Redefine in
+        // closed form.
+        let value = averagePooledDF(kernelSize: kernelSize, strides: strides,
+                                  padding: padding, dataFormat: dataFormat)
+        return (value, { v in
+            return Raw.avgPoolGrad(
+                origInputShape: self.shapeTensor,
+                grad: v,
+                ksize: [Int32(kernelSize.0), Int32(kernelSize.1),
+                        Int32(kernelSize.2), Int32(kernelSize.3)],
+                strides: [Int32(strides.0), Int32(strides.1), Int32(strides.2), Int32(strides.3)],
+                padding: padding.raw,
+                dataFormat: dataFormat
+            )
+        })
+    }
+}
+public extension Tensor where Scalar: FloatingPoint {
+    /// Computes a 2-D convolution using `self` as input, with the specified
+    /// filter, strides, and padding.
+    ///
+    /// - Parameters:
+    ///     - filter: The convolution filter.
+    ///     - strides: The strides of the sliding filter for each dimension of the
+    ///         input.
+    ///     - padding: The padding for the operation.
+    /// - Precondition: `self` must have rank 4.
+    /// - Precondition: `filter` must have rank 4.
+    @inlinable @inline(__always)
+    @differentiable(
+        wrt: (self, filter), vjp: _vjpConvolved2DDF
+        where Scalar: TensorFlowFloatingPoint
+    )
+    func convolved2DDF(
+        withFilter filter: Tensor,
+        strides: (Int, Int, Int, Int),
+        padding: Padding,
+        dataFormat: Raw.DataFormat = .nhwc
+    ) -> Tensor {
+        return Raw.conv2D(
+            self,
+            filter: filter,
+            strides: [Int32(strides.0), Int32(strides.1), Int32(strides.2), Int32(strides.3)],
+            padding: padding.raw2,
+            explicitPaddings: [],
+            dataFormat: dataFormat
+        )
+    }
+    
+    @inlinable @inline(__always)
+    @differentiable(
+        wrt: self, vjp: _vjpAveragePooledDF(kernelSize:strides:padding:dataFormat:)
+        where Scalar : TensorFlowFloatingPoint
+    )
+    func averagePooledDF(
+        kernelSize: (Int, Int, Int, Int),
+        strides: (Int, Int, Int, Int),
+        padding: Padding,
+        dataFormat: Raw.DataFormat = .nhwc
+    ) -> Tensor {
+        return Raw.avgPool(
+            value: self,
+            ksize: [Int32(kernelSize.0), Int32(kernelSize.1),
+                    Int32(kernelSize.2), Int32(kernelSize.3)],
+            strides: [Int32(strides.0), Int32(strides.1), Int32(strides.2), Int32(strides.3)],
+            padding: padding.raw,
+            dataFormat: dataFormat)
+    }
 }
--- Tests/LayersDataFormatTests/LayersDataFormatTests.swift
+++ Tests/LayersDataFormatTests/LayersDataFormatTests.swift
@@ -1,15 +1,43 @@
 import XCTest
+import TensorFlow
 @testable import LayersDataFormat
 
 final class LayersDataFormatTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(LayersDataFormat().text, "Hello, World!")
+    func testAveragePooledDF() {
+        let input = Tensor<Float>(randomNormal: [12, 16, 16, 3])
+        let targetNHWC = avgPool2D(input,
+                               filterSize: (1, 2, 2, 1),
+                               strides: (1, 2, 2, 1),
+                               padding: .same)
+        XCTAssertEqual(input.averagePooledDF(kernelSize: (1, 2, 2, 1),
+                                             strides: (1, 2, 2, 1),
+                                             padding: .same,
+                                             dataFormat: .nhwc),
+                       targetNHWC)
+        /* TODO. Uncomment .nchw test that will only be run on GPU.
+        XCTAssertEqual(input.transposed(withPermutations: [0, 3, 1, 2])
+                            .averagePooledDF(kernelSize: (1, 2, 2, 1),
+                                             strides: (1, 2, 2, 1),
+                                             padding: .same,
+                                             dataFormat: .nchw),
+                       targetNHWC.transposed(withPermutations: [0, 3, 1, 2]))
+        */
+    }
+    
+    func testConvolved2DDF() {
+        let input = Tensor<Float>(randomNormal: [12, 16, 16, 3])
+        let filter = Tensor<Float>(randomNormal: [3, 3, 3, 16])
+        let targetNHWC = conv2D(input, filter: filter, strides: (1, 1, 1, 1), padding: .same)
+        XCTAssertEqual(input.convolved2DDF(withFilter: filter,
+                                           strides: (1,1,1,1),
+                                           padding: .same,
+                                           dataFormat: .nhwc),
+                       targetNHWC)
+        
+        // TODO. Add .nchw test that will only be run on GPU.
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testConvolved2DDF", testConvolved2DDF),
     ]
 }
